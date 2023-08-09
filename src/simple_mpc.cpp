db398cbd3ac2103e9d478b0d7c610455c3054b95
Tatsuhiro Ikebe <beike315@icloud.com>
2023-08-09 22:40:01 +0900

    Add predictive horizon curve path

--- src/simple_mpc.cpp
+++ src/simple_mpc.cpp
@@ -15,70 +15,86 @@ using ceres::Solver;
 
 using namespace sciplot;
 
-const int N = 20;
-const double dt = 0.1;
+const int N = 40;
+const double dt = 1;
 
-std::vector<double> path_xy;
+std::vector<double> path_x;
+std::vector<double> path_y;
 
-struct RobotModel
+struct ObjectiveFunction
 {
-  RobotModel(double dt, std::vector<double> path_xy) : dt_(dt), path_xy_(path_xy) {}
+  ObjectiveFunction(double dt, std::vector<double> path_x, std::vector<double> path_y)
+  : dt_(dt), path_x_(path_x), path_y_(path_y)
+  {
+  }
 
   template <typename T>
   bool operator()(const T * const v, const T * const w, T * residual) const
   {
-    static std::vector<T> xs;
-    static std::vector<T> ys;
-    static std::vector<T> ths;
-    static std::vector<T> costs;
-
-    xs.push_back((T)0.0);
-    ys.push_back((T)0.0);
-    ths.push_back((T)0.00000000000000000000001);
+    std::vector<T> xs(N + 1, (T)0.0);
+    std::vector<T> ys(N + 1, (T)0.0);
+    std::vector<T> ths(N + 1, (T)0.0);
 
     for (int i = 0; i < N; i++) {
       // clang-format off
-      T x =
-          xs[i] + 
-            (v[i] / w[i]) * 
-              (sin(ths[i] + w[i] * dt_) - sin(ths[i])) ;
-      T y = 
-          ys[i] +
-            (v[i] / w[i]) * 
-              ((T)-1.0 * cos(ths[i] + w[i] * dt_) + cos(ths[i]));
-      T th = 
-          ths[i] + 
-            w[i] * dt_;
+      // if (w[i] >= (T)1.0e-100) {
+        // x =
+        //     xs[i] + 
+        //       (v[i] / w[i]) * 
+        //         (sin(ths[i] + w[i] * dt_) - sin(ths[i]));
+        // y = 
+        //     ys[i] +
+        //       (v[i] / w[i]) * 
+        //         ((T)-1.0 * cos(ths[i] + w[i] * dt_) + cos(ths[i]));
+        // th = 
+        //     ths[i] + 
+        //       w[i] * dt_;
+      // }
+      // if(w[i] <= (T)1.0e-100) {
+      T  x =
+            xs[i] + 
+              v[i] * cos(ths[i]);
+      T  y = 
+            ys[i] +
+              v[i] * sin(ths[i]);
+      T  th = 
+            ths[i] + 
+              w[i] * dt_;
+      // }
+
       // clang-format on
 
-      T cost = sqrt(pow((path_xy_[i * 2] - x), 2) + pow((path_xy_[i * 2 + 1] - y), 2));
+      T cost = pow((path_x_[i] - x), 2) + pow((path_y_[i] - y), 2);
 
-      xs.push_back(x);
-      ys.push_back(y);
-      ths.push_back(th);
-      costs.push_back(cost);
+      xs[i + 1] = x;
+      ys[i + 1] = y;
+      ths[i + 1] = th;
+
+      // cost += cost;
 
       residual[i] = cost;
     }
+
+    // residual[0] = accumulate(costs.begin(), costs.end(), (T)0.0);
+    // residual[0] = cost;
 
     return true;
   }
 
 private:
   double dt_;
-  std::vector<double> path_xy_;
+  std::vector<double> path_x_;
+  std::vector<double> path_y_;
 };
 
 int main(int argc, char ** argv)
 {
   google::InitGoogleLogging(argv[0]);
 
-  path_xy.reserve(2 * N);
-  for (int i = 0; i <= N; ++i) {
-    double x = i;
-    double y = std::sin(x);
-    path_xy.push_back(x);
-    path_xy.push_back(y);
+  for (int i = 0; i <= 100; ++i) {
+    double x = static_cast<double>(i);
+    path_x.push_back(x / 10);
+    path_y.push_back(sin(x / 10));
   }
 
   std::vector<double> v_in(N, 0.0);
@@ -88,12 +104,13 @@ int main(int argc, char ** argv)
 
   Problem problem;
   problem.AddResidualBlock(
-    new AutoDiffCostFunction<RobotModel, N, N, N>(new RobotModel(dt, path_xy)), nullptr,
-    v_out.data(), w_out.data());
-  const double lower_bound = 1.0e-10;
+    new AutoDiffCostFunction<ObjectiveFunction, N, N, N>(new ObjectiveFunction(dt, path_x, path_y)),
+    nullptr, v_out.data(), w_out.data());
   for (int i = 0; i < N; ++i) {
-    problem.SetParameterLowerBound(v_out.data(), i, lower_bound);
-    problem.SetParameterLowerBound(w_out.data(), i, lower_bound);
+    problem.SetParameterLowerBound(v_out.data(), i, 1.0e-10);
+    problem.SetParameterLowerBound(w_out.data(), i, -3.14);
+    problem.SetParameterUpperBound(v_out.data(), i, 1.0);
+    problem.SetParameterUpperBound(w_out.data(), i, 3.14);
   }
 
   Solver::Options options;
@@ -118,6 +135,43 @@ int main(int argc, char ** argv)
   for (auto omega : w_out) std::cout << omega << ", ";
   std::cout << "\n";
 
+  std::vector<double> predictive_horizon_x;
+  std::vector<double> predictive_horizon_y;
+  std::vector<double> ths;
+
+  predictive_horizon_x.push_back((double)0.0);
+  predictive_horizon_y.push_back((double)0.0);
+  ths.push_back((double)0.0);
+
+  for (int i = 0; i < N; i++) {
+    // clang-format off
+      // double x =
+      //     predictive_horizon_x[i] + 
+      //       (v_out[i] / w_out[i]) * 
+      //         (sin(ths[i] + w_out[i] * dt) - sin(ths[i])) ;
+      // double y = 
+      //     predictive_horizon_y[i] +
+      //       (v_out[i] / w_out[i]) * 
+      //         (-1.0 * cos(ths[i] + w_out[i] * dt) + cos(ths[i]));
+      // double th = 
+      //     ths[i] + 
+      //       w_out[i] * dt;
+      double  x =
+            predictive_horizon_x[i] + 
+              v_out[i] * cos(ths[i]);
+      double  y = 
+            predictive_horizon_y[i] +
+              v_out[i] * sin(ths[i]);
+      double  th = 
+            ths[i] + 
+              w_out[i] * dt;
+    // clang-format on
+
+    predictive_horizon_x.push_back(x);
+    predictive_horizon_y.push_back(y);
+    ths.push_back(th);
+  }
+
   Plot2D plot;
   plot.fontName("Palatino");
   plot.xlabel("x").fontSize(20);
@@ -125,16 +179,10 @@ int main(int argc, char ** argv)
   plot.legend().atTop().fontSize(20).displayHorizontal().displayExpandWidthBy(2);
   plot.grid().show();
 
-  std::vector<double> x;
-  std::vector<double> y;
-  double cnt = 0;
-  for (int i = 0; i <= 100; ++i) {
-    x.push_back(static_cast<double>(cnt));
-    y.push_back(std::sin(cnt));
-    cnt += (6.28 / 100);
-  }
-
-  plot.drawCurve(x, y).label("path").lineColor("green");
+  plot.drawCurve(path_x, path_y).label("Path").lineColor("green");
+  plot.drawCurve(predictive_horizon_x, predictive_horizon_y)
+    .label("Predictive Horizon")
+    .lineColor("red");
 
   Figure fig = {{plot}};
   Canvas canvas = {{fig}};
